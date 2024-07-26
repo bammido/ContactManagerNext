29abcc9acdee52ea189acb37d67f24bd20e43587
Bryan Fernandes de Oliveira <65303066+bammido@users.noreply.github.com>
2024-07-26 10:32:07 -0300

    Update navigation.tsx
    ajuste de cores

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -23,7 +23,7 @@ export default function HomeNav() {
 
     return <>
         { pathname !== '/login' && <section>
-            <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <nav className="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <div className="items-center justify-between w-full flex" id="navbar-sticky">
                         <ul className="flex p-4 font-medium space-x-8 rtl:space-x-reverse mt-0 border-0">
